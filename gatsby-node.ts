99a88394c44993a9b1bf8d3fb4e66a646d3de7fa
cieloazul310 <cieloazul310@gmail.com>
2022-08-31 20:39:46 +0900

    add `gatsby-node-ts`

---
+++ gatsby-node.ts
@@ -0,0 +1,21 @@
+// import type { CreatePagesArgs, CreateResolversArgs, CreateNodeArgs, CreateSchemaCustomizationArgs } from 'gatsby';
+/*
+export function onCreateNode({ node, actions }: CreateNodeArgs) {
+  const { createNodeField } = actions;
+}
+*/
+/*
+export function createSchemaCustomization({ actions }: CreateSchemaCustomizationArgs) {
+  const { createTypes } = actions;
+}
+*/
+/*
+export function createResolvers(actions: CreateResolversArgs) {
+  
+}
+*/
+/*
+export async function createPages({ actions, graphql, reporter }: CreatePagesArgs) {
+  const { createPage } = actions;
+}
+*/
